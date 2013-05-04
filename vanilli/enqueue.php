b94c3516d8d0a454af62cb2f3c8f895419458911
Paul Graham <paul@bytefair.com>
2013-05-04 11:15:50 -0500

    brings some more clarity to the enqueue functions

--- vanilli/enqueue.php
+++ vanilli/enqueue.php
@@ -2,32 +2,37 @@
 /*
 enqueue.php
 
-Includes the style and script defnitions
+Includes the style and script defnitions.
  */
 
-add_action( 'wp_enqueue_scripts', 'v_enqueues', 999 );
-function v_enqueues() {
-	if (!is_admin()) {
-		// I'm using the Bower version, you should customize this for production
-		wp_register_script(
-			'modernizr',
-			get_stylesheet_directory_uri() . '/components/modernizr/modernizr.js',
-			array(),
-			'2.6.2',
-			false );
+add_action( 'wp_enqueue_scripts', 'v_site_enqueues', 999 );
+function v_site_enqueues() {
+	/*** REGISTER ***/
 
-		// Why style.css only? We're compiling it, so why not?
-		wp_enqueue_style(
-			'style',
-			get_stylesheet_uri(),
-			array(),
-			VERSION,
-			'all' );
-		wp_enqueue_script( 'modernizr' );
-		// Use a plugin to load from CDN if you want, make sure it loads in
-		// noConflict mode for compatibility
-		wp_enqueue_script( 'jquery' );
-	}
+	// I'm using the Bower version, you should customize this for production by
+	// going to http://modernizr.com/download/ and only checking the things you
+	// will actually need.
+	wp_register_script(
+		'modernizr',
+		get_stylesheet_directory_uri() . '/components/modernizr/modernizr.js',
+		array(),
+		'2.6.2',
+		false );
+
+	/*** QUEUE ***/
+
+	// Let's use Sass and compile into one stylsheet. Compress for production.
+	wp_enqueue_style(
+		'style',
+		get_stylesheet_uri(),
+		array(),
+		VERSION,
+		'all' );
+	wp_enqueue_script( 'modernizr' );
+	// Use a plugin to load from CDN if you want, make sure it loads in
+	// noConflict mode for compatibility. Probably better to just use native
+	// if you can afford the bandwidth.
+	// wp_enqueue_script( 'jquery' );
 }
 
 ?>
