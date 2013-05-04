923a95d9be543d2a0af4df399f0a9299a594a7cb
Paul Graham <paul@bytefair.com>
2013-05-03 23:33:17 -0500

    more basic theme fleshing

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<!-- code functions -->
+		<?php wp_head(); ?>
+		<!-- /code functions -->
+
+	</head>
+	<body <?php body_class(); ?>>
+
+
+
+	</body>
+
+	<!-- code functions -->
+	<?php wp_footer(); ?>
+	<!-- /code functions -->
+
+</html>
---
+++ sass/readme.md
@@ -0,0 +1,16 @@
+# Vanilli, an aggro WordPress template for real HTML g's
+
+## Uses
+
+HTML5 Boilerplate
+Bones
+Normalize.css
+
+## TODO
+
+0.1.0 - Basic page structure works, no CSS, code imported
+0.2.0 - Skeleton CSS
+0.3.0 - JavaScript systems set up
+0.4.0 - Fancy CSS
+0.5.0 - Custom functionality and plugins?
+1.0.0 - release
