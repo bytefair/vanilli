43f1c79c72c5dfb53a6c69bf895a7667ace8b71f
Paul Graham <paul@bytefair.com>
2013-05-04 12:38:02 -0500

    scaffolding

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+<article class="hentry" <?php post_class( 'clearfix' ); ?> role="article">
+
+	<header>
+		<h1 class="entry-title">
+			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+		<h2>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+
+</article>
+
 <?php endwhile; ?>
+
 <?php endif; ?>
 <?php get_footer(); ?>
