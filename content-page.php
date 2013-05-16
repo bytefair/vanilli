d34783ae927a1a83f732a4e0ab9ed938a64af20b
Paul Graham <pgraham@cordelllaw.com>
2013-05-16 12:34:25 -0500

    provides a page template

---
+++ content-page.php
@@ -0,0 +1,14 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+
+	<header>
+		<h1 class="entry-title"><?php
+				the_title(); ?>
+		</h1>
+	</header>
+
+	<div class="entry-content clearfix"><?php
+		the_content();
+		wp_link_pages(); ?>
+	</div>
+
+</article>
