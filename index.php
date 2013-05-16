262414b4925267150dd82af32dcf8b483e65b9cd
Paul Graham <pgraham@cordelllaw.com>
2013-05-16 09:43:27 -0500

    more default article fleshing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<article class="hentry" <?php post_class( 'clearfix' ); ?> role="article">
+<article id="post-<?php the_ID(); ?>" class="hentry" <?php post_class( 'clearfix' ); ?> role="article">
 
 	<header>
 		<h1 class="entry-title">
@@ -10,21 +10,24 @@
 			</a>
 		</h1>
 		<h2 class="entry-summary"></h2>
-		<p class="byline vcard">
-			<?php printf(
-							__( 'Posted on <time class="published" datetime="%1$s">%2$s</time> by <span class="author fn">%3$s</span>', 'vanilli' ),
-							get_the_time( 'Y-m-j' ),
-							get_the_time( get_option( 'date_format' ) ),
-							v_get_the_author_posts_link() ); ?>
+		<p class="byline vcard"><?php
+			printf(
+				__( 'Posted on <time class="published" datetime="%1$s">%2$s</time> by <span class="author fn">%3$s</span>', 'vanilli' ),
+				get_the_time( 'Y-m-j' ),
+				get_the_time( get_option( 'date_format' ) ),
+				v_get_the_author_posts_link() ); ?>
+		</p>
+		<p class="post-category"><?php
+			printf(__( 'Filed under: %1$s', 'vanilli' ), get_the_category_list( ', ' )); ?>
 		</p>
 	</header>
 
-	<div class="entry-content">
+	<div class="entry-content clearfix">
 		<?php the_content(); ?>
 	</div>
 
-	<footer>
-
+	<footer><?php
+			the_tags(); ?>
 	</footer>
 
 </article>
