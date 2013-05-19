2fb32b5f1d52ead608cd285b1f7bc969c4782521
Paul Graham <paul@bytefair.com>
2013-05-19 13:49:17 -0500

    basic 404

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+/*
+404.php
+
+This template is thrown on 404.
+ */
+get_header(); ?>
+
+<h1><?
+  _e( '404 - Where are you trying to go?', 'vanilli' ); ?>
+</h1>
+
+<div class="entry-content clearfix"><?php
+  /*
+  The 404 is such a good opportunity for you to do cool marketing or have some
+  fun, and here you are throwing a search form. You should probably replace the
+  functionlity here on your own site.
+   */
+  get_search_form(); ?>
+
+</div><?php
+
+get_footer(); ?>
