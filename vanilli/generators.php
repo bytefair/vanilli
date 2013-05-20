1c2f3d6f3f6bdef187f6eb9a4c3c7a838d28ad47
Paul Graham <pgraham@cordelllaw.com>
2013-05-20 13:57:43 -0500

    minor style changes

--- vanilli/generators.php
+++ vanilli/generators.php
@@ -40,7 +40,8 @@ function v_get_the_author_posts_link() {
 	$link = sprintf(
 		'<a href="%1$s" title="%2$s" rel="author">%3$s</a>',
 		get_author_posts_url( $authordata->ID, $authordata->user_nicename ),
-		esc_attr( sprintf( __( 'Posts by %s' ), get_the_author() ) ), // No further l10n needed, core will take care of this one
+		// No further l10n needed, core will take care of this one
+		esc_attr( sprintf( __( 'Posts by %s' ), get_the_author() ) ),
 		get_the_author()
 	);
 	return $link;
