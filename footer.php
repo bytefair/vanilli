5191ba69d4594bc8725635e2a2866e1227a5357b
Paul Graham <paul@bytefair.com>
2013-05-26 20:00:39 -0500

    credits

--- footer.php
+++ footer.php
@@ -5,6 +5,9 @@
 					<div class="source-org copyright">
 						&copy; <?php echo date('Y'); ?> <span class="org fn"><?php bloginfo( 'name' ); ?></span>.
 					</div>
+					<div class="theme-credits">
+						Handcarved with love using <a href="http://wordpress.org">WordPress</a> and <a href="https://github.com/bytefair/vanilli">Vanilli</a>.
+					</div>
 				</div>
 			</footer>
 		</div><!-- /.site-wrapper -->
