f957984ddf72a48ea1cd38c8b542bc1a42d9085a
Paul Graham <paul@bytefair.com>
2013-05-26 21:51:04 -0500

    adds call for featured image

--- content-single.php
+++ content-single.php
@@ -1,6 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="hentry" <?php post_class(); ?> role="article">
 
-	<header>
+	<header><?php
+		the_post_thumbnail(); ?>
 		<h1 class="entry-title">
 			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php
 				the_title(); ?>
