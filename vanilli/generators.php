d55bcca995a56914de804a6724fc46d69c402d08
Paul Graham <pgraham@cordelllaw.com>
2013-05-28 09:33:26 -0500

    some cleanup of comment styles

--- vanilli/generators.php
+++ vanilli/generators.php
@@ -150,7 +150,7 @@ function v_comments($comment, $args, $depth) {
 			<header class="comment-author vcard"><?php
 				// create variable
 				$bgauthemail = get_comment_author_email();?>
-				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" /><?php
+				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/images/nothing.gif" /><?php
 				printf(__('<cite class="fn">%s</cite>', 'vanilli'), get_comment_author_link()) ?>
 				<time datetime="<?php echo comment_time('Y-m-j'); ?>">
 					<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php
