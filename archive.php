e01021f2f8e4919406cb71fe1debb6358cdad54a
Paul Graham <paul@bytefair.com>
2013-06-03 20:03:46 -0500

    fixes the out of boundary header

--- archive.php
+++ archive.php
@@ -4,21 +4,21 @@ archive.php
 
 Controls display of archives
  */
-get_header();
-/** controls the complicated **/
-echo v_archive_header(); ?>
+get_header(); ?>
 <div class="site-content"><?php
-  if (have_posts()) {
-  	/** start the loop **/
-  	while (have_posts()) {
-  		/** unfurls the currently iterated post so you can call template tags **/
-  		the_post();
-  		/** calls content-{format}.php or falls back to content.php **/
-  		get_template_part( 'content', 'archive' );
-  	}
-  	/** end the loop **/
-  	do_action( 'v_pagination' );
-  } else get_template_part( 'no-results', 'archive' ); ?>
+	/** controls the complicated archive title **/
+	echo v_archive_header();
+	if (have_posts()) {
+		/** start the loop **/
+		while ( have_posts() ) {
+			/** unfurls the currently iterated post so you can call template tags **/
+			the_post();
+			/** calls content-{format}.php or falls back to content.php **/
+			get_template_part( 'content', 'archive' );
+		}
+		/** end the loop **/
+		do_action( 'v_pagination' );
+	} else get_template_part( 'no-results', 'archive' ); ?>
 </div><?php
 get_sidebar();
 get_footer(); ?>
