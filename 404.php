d3f58b9d9fa7735a9cc1f29c01e057bfe83cdc63
Paul Graham <pgraham@cordelllaw.com>
2013-06-05 08:30:39 -0500

    title outside boundary

--- 404.php
+++ 404.php
@@ -6,17 +6,17 @@ This template is thrown on 404.
  */
 get_header(); ?>
 
-<h1><?
-  _e( '404 - Where are you trying to go?', 'vanilli' ); ?>
-</h1>
-
-<div class="entry-content"><?php
-  /*
-  The 404 is such a good opportunity for you to do cool marketing or have some
-  fun, and here you are throwing a search form. You should probably replace the
-  functionlity here on your own site.
-   */
-  get_search_form(); ?>
+<div class="entry-content">
+	<h1><?
+		_e( '404 - Where are you trying to go?', 'mens' ); ?>
+	</h1>
+	<?php
+	/*
+	The 404 is such a good opportunity for you to do cool marketing or have some
+	fun, and here you are throwing a search form. You should probably replace the
+	functionlity here on your own site.
+	 */
+	get_search_form(); ?>
 
 </div><?php
 
