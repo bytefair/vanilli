07c348f9962de6d774bce20f1ba005cbc7f2e2d0
Paul Graham <pgraham@cordelllaw.com>
2013-06-05 08:32:07 -0500

    style edit

--- 404.php
+++ 404.php
@@ -9,8 +9,7 @@ get_header(); ?>
 <div class="entry-content">
 	<h1><?
 		_e( '404 - Where are you trying to go?', 'mens' ); ?>
-	</h1>
-	<?php
+	</h1><?php
 	/*
 	The 404 is such a good opportunity for you to do cool marketing or have some
 	fun, and here you are throwing a search form. You should probably replace the
