91ff627ad655b93b0acd8dafdf6a1358d80c3822
Paul Graham <paul@bytefair.com>
2013-06-11 21:56:47 -0500

    some style refactoring for clarity

--- comments.php
+++ comments.php
@@ -6,16 +6,18 @@ fan of the way WP's default structure is.
 They are laregly taken from Bones.
  */
 
-// safety checks, do not remove these unless you know what you're doing
+/*** SECURITY CHECKS ***/
+
 if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 	die ('Please do not load this page directly. Thanks!');
 
 if ( post_password_required() )
 	return;
 
-/** comment form starts here **/
+/*** DISPLAY COMMENTS ***/
 
 if ( have_comments() ) : ?>
+
 	<h2 class="commments-title"><?php
 		printf(
 			_nx(
@@ -27,7 +29,9 @@ if ( have_comments() ) : ?>
 			number_format_i18n( get_comments_number() ),
 			'<span>' . get_the_title() . '</span>' ); ?>
 	</h2><?php
+
 	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+
 		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
 			<h1 class="screen-reader-text"><?php
 				_e( 'Navigate comments', 'vanilli' ); ?>
@@ -39,11 +43,15 @@ if ( have_comments() ) : ?>
 				next_comments_link( __( 'Newer Comments &rarr;', 'vanilli' ) ); ?>
 			</div>
 		</nav><?php
+
 	endif; ?>
+
 	<ol class="commentlist">
 		<?php wp_list_comments('type=comment&callback=v_list_comments'); ?>
 	</ol><?php
+
 	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+
 		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
 			<h1 class="screen-reader-text"><?php
 				_e( 'Navigate comments', 'vanilli' ); ?>
@@ -55,14 +63,23 @@ if ( have_comments() ) : ?>
 				next_comments_link( __( 'Newer Comments &rarr;', 'vanilli' ) ); ?>
 			</div>
 		</nav><?php
+
 	endif;
+
 else:
-	if ( comments_open() ) : // comments are open, but there are no comments
-	else : // comments are closed
+
+	if ( comments_open() ) :
+		// comments are open, but there are no comments
+	else :
+		// comments are closed
 	endif;
-endif; // end have_comments();
+
+endif;
+
+/*** COMMENT FORM ***/
 
 if ( comments_open() ) : ?>
+
 	<section id="respond" class="respond-form">
 		<h3><?php
 			comment_form_title(
@@ -78,6 +95,7 @@ if ( comments_open() ) : ?>
 		</div><?php
 
 		if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
+
 			<div class="alert alert-help">
 				<p><?php
 					printf(
@@ -86,45 +104,43 @@ if ( comments_open() ) : ?>
 						'</a>'
 					); ?></p>
 			</div><?php
+
 		else : ?>
+
 			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform"><?php
-				if ( is_user_logged_in() ) : ?>
+
+				if ( is_user_logged_in() ) ?>
 
 					<p class="comments-logged-in-as"><?php _e("Logged in as", "vanilli"); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e("Log out of this account", "vanilli"); ?>"><?php _e("Log out", "vanilli"); ?> <?php _e("&raquo;", "vanilli"); ?></a></p><?php
 
-				else : // this happens if user is NOT logged in ?>
+				else : ?>
 
-					<ul class="clearfix">
-
+					<ul>
 						<li>
 							<label for="author"><?php _e("Name", "vanilli"); ?> <?php if ($req) _e("(required)"); ?></label>
 							<input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
 						</li>
-
 						<li>
 							<label for="email"><?php _e("Mail", "vanilli"); ?> <?php if ($req) _e("(required)"); ?></label>
 							<input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
 							<small><?php _e("(will not be published)", "vanilli"); ?></small>
 						</li>
-
 						<li>
 							<label for="url"><?php _e("Website", "vanilli"); ?></label>
 							<input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" tabindex="3" />
 						</li>
-
 					</ul><?php
-				endif; //end user is logged in ?>
+
+				endif; ?>
 
 				<p>
 					<textarea name="comment" id="comment" tabindex="4">
 					</textarea>
 				</p>
-
 				<p>
 					<input name="submit" type="submit" id="submit" class="button" tabindex="5" value="<?php _e('Submit', 'vanilli'); ?>" /><?php
 					comment_id_fields(); ?>
 				</p>
-
 				<div class="alert alert-help">
 					<p class="allowed-tags">
 						<strong>XHTML:</strong> <?php _e('You can use these tags', 'vanilli'); ?>: <code><?php echo allowed_tags(); ?></code>
@@ -134,7 +150,9 @@ if ( comments_open() ) : ?>
 				do_action('comment_form', $post->ID); ?>
 
 			</form><?php
-		endif; // end registration and not logged in ?>
+
+		endif; ?>
+
 	</section><?php
-endif; //end comments open
-?>
+
+endif; ?>
